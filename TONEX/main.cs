29736e4d5fe85b95102ee5cbf4c50047428cefc0
Slok7565 <Slok7565@qq.com>
2024-07-26 19:08:29 +0800

    Last Commit

--- TONEX/main.cs
+++ TONEX/main.cs
@@ -41,9 +41,9 @@ public class Main : BasePlugin
     // == 版本相关设定 / Version Config ==
     public const string LowestSupportedVersion = "2024.6.18";
     public static readonly bool IsPublicAvailableOnThisVersion = true;
-    public const string PluginVersion = "1.3.2.3";
-    public const string ShowVersion_Head = "1.3_20240724";
-    public const string ShowVersion_TestText = "_Debug_53";
+    public const string PluginVersion = "1.3.5";
+    public const string ShowVersion_Head = "1.3_20240726";
+    public const string ShowVersion_TestText = "";
     public const string ShowVersion = ShowVersion_Head + ShowVersion_TestText;
     public const int PluginCreation = 1;
     // == 链接相关设定 / Link Config ==
