9e328fbd76569bdffe9b5f83f212db8a18a4c6a2
Slok7565 <Slok7565@qq.com>
2024-08-17 21:24:36 +0800

    WOO

--- TONEX/main.cs
+++ TONEX/main.cs
@@ -42,7 +42,7 @@ public class Main : BasePlugin
     public const string LowestSupportedVersion = "2024.6.18";
     public static readonly bool IsPublicAvailableOnThisVersion = true;
     public const string PluginVersion = "1.3.6";
-    public const string ShowVersion_Head = "1.3_2024814";
+    public const string ShowVersion_Head = "1.3_20240814";
     public const string ShowVersion_TestText = "";
     public const string ShowVersion = ShowVersion_Head + ShowVersion_TestText;
     public const int PluginCreation = 1;
